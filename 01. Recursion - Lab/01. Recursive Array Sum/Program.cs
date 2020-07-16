d99946d138a452404f8abb01e08c493edd3b39ac
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-07-16 17:52:13 +0300

    Add "01. Recursion - Lab" exercises { 01, 02 }
    "01. Recursive Array Sum ",
    "02. Recursive Factorial "

---
+++ 01. Recursion - Lab/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01._Recursive_Array_Sum
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static int Sum(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[index] + Sum(arr, index + 1);
+        }
+
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(Sum(inputNumbers, 0).ToString());
+        }
+    }
+}
---
+++ 01. Recursion - Lab/02. Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Recursive_Factorial
+{
+    class Program
+    {
+        static long Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+
+            return num * Factorial(num - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            var inputNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(inputNumber));
+        }
+    }
+}
