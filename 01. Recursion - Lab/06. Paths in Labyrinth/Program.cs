d0b4968b97b6a3b4263da3109a842cd3abaa7185
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-12-30 15:32:55 +0200

    Add Recursion - Lab

---
+++ 01. Recursion - Lab/06. Paths in Labyrinth/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Paths_in_Labyrinth
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            var lab = new char[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var line = Console.ReadLine();
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    lab[i, j] = line[j];
+                }
+            }
+
+            var directions = new List<char>();
+            FindAllPaths(lab, 0, 0, directions, '\0');
+        }
+
+        private static void FindAllPaths(char[,] lab, int row, int col, List<char> directions, char direction)
+        {
+            if (IsOutside(lab, row, col) || 
+                IsWall(lab, row, col) ||
+                IsVisited(lab, row, col))
+            {
+                return;
+            }
+
+            directions.Add(direction);
+
+            if (IsSolution(lab, row, col))
+            {
+                Console.WriteLine(string.Join("", directions));
+                directions.RemoveAt(directions.Count - 1);
+                return;
+            }
+
+            lab[row, col] = 'v';
+
+            FindAllPaths(lab, row - 1, col, directions, 'U') ;
+            FindAllPaths(lab, row + 1, col, directions, 'D');
+            FindAllPaths(lab, row, col - 1, directions, 'L');
+            FindAllPaths(lab, row, col + 1, directions, 'R');
+
+            directions.RemoveAt(directions.Count - 1);
+            lab[row, col] = '-';
+        }
+
+        private static bool IsSolution(char[,] lab, int row, int col)
+        {
+            return lab[row, col] == 'e';
+        }
+
+        private static bool IsVisited(char[,] lab, int row, int col)
+        {
+            return lab[row, col] == 'v';
+        }
+
+        private static bool IsWall(char[,] lab, int row, int col)
+        {
+            return lab[row, col] == '*';
+        }
+
+        private static bool IsOutside(char[,] lab, int row, int col)
+        {
+            return row < 0 ||
+                   row >= lab.GetLength(0) ||
+                   col < 0 ||
+                   col >= lab.GetLength(1);
+        }
+    }
+}
---
+++ 01. Recursion - Lab/08. Recursive Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Recursive_Fibonacci
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFibonacci(number));
+        }
+
+        private static int GetFibonacci(int number)
+        {
+            int result = 0;
+            int previous = 1;
+
+            for (int i = 0; i <= number; i++)
+            {
+                int temp = result;
+                result = previous;
+                previous = temp + previous;
+            }
+
+            return result;
+        }
+    }
+}
