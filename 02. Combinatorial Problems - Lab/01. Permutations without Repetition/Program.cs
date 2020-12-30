d47ece85dc97ea5c18151fb8253804c6b2783d13
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-12-30 18:49:28 +0200

    Add 02. Combinatorial Problems - Lab All Exercises

---
+++ 02. Combinatorial Problems - Lab/01. Permutations without Repetition/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Permutations_without_Repetition
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Time complexity: n! *n | Memory complexity 3 * N
+            var allPermutationsWithMoreMemoryComplexity = new WithMoreMemory();
+            allPermutationsWithMoreMemoryComplexity.AllPermutations();
+
+            // Time complexity: n! *n | Memory complexity N
+            var allPermutationsOptimized = new WithLessMemory();
+            allPermutationsOptimized.AllPermutations();
+        }
+    }
+}
---
+++ 02. Combinatorial Problems - Lab/01. Permutations without Repetition/WithMoreMemory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01._Permutations_without_Repetition
+{
+    public class WithMoreMemory
+    {
+        private static string[] elements;
+        private static string[] permutations;
+        private static bool[] used;
+
+        public void AllPermutations()
+        {
+            elements = Console.ReadLine().Split(" ");
+            permutations = new string[elements.Length];
+            used = new bool[elements.Length];
+
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    permutations[index] = elements[i];
+
+                    Permute(index + 1);
+
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
