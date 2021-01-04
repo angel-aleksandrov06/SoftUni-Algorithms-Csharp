eb22a57967a5d3d87412288e3019d05b631c7621
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2021-01-05 00:15:17 +0200

    Add 06. Graph Theory, Traversal and Shortest Paths - Exercise

---
+++ 06. Graph Theory, Traversal and Shortest Paths - Exercise/02. Areas in Matrix/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace _02._Areas_in_Matrix
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Node
+    {
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+    }
+
+    public class Program
+    {
+        private static char[,] matrix;
+        private static bool[,] visited;
+
+        static void Main(string[] args)
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            matrix = ReadMatrix(rows, cols);
+            visited = new bool[rows, cols];
+
+            var areas = new SortedDictionary<char, int>();
+            var totalAreas = 0;
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    if (visited[r,c])
+                    {
+                        continue;
+                    }
+
+                    DFS(r, c);
+
+                    totalAreas++;
+
+                    if (!areas.ContainsKey(matrix[r,c]))
+                    {
+                        areas.Add(matrix[r, c], 1);
+                    }
+                    else
+                    {
+                        areas[matrix[r, c]]++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Areas: {totalAreas}");
+
+            foreach (var area in areas)
+            {
+                Console.WriteLine($"Letter '{area.Key}' -> {area.Value}");
+            }
+        }
+
+        private static void DFS(int row, int col)
+        {
+            visited[row, col] = true;
+
+            var children = GetChildren(row, col);
+
+            foreach (var child in children)
+            {
+                DFS(child.Row, child.Col);
+            }
+        }
+
+        private static List<Node> GetChildren(int row, int col)
+        {
+            var children = new List<Node>();
+
+            if (IsInside(row - 1, col) && IsChild(row, col, row-1, col) && !IsVisited(row -1, col))
+            {
+                children.Add(new Node { Row = row - 1, Col = col});
+            }
+
+            if (IsInside(row + 1, col) && IsChild(row, col, row + 1, col) && !IsVisited(row + 1, col))
+            {
+                children.Add(new Node { Row = row + 1, Col = col });
+            }
+
+            if (IsInside(row, col -1) && IsChild(row, col, row, col - 1) && !IsVisited(row, col - 1))
+            {
+                children.Add(new Node { Row = row, Col = col - 1 });
+            }
+
+            if (IsInside(row, col + 1) && IsChild(row, col, row, col + 1) && !IsVisited(row, col + 1))
+            {
+                children.Add(new Node { Row = row, Col = col + 1 });
+            }
+
+            return children;
+        }
+
+        private static bool IsVisited(int row, int col)
+        {
+            return visited[row, col];
+        }
+
+        private static bool IsChild(int parentRow, int parentCol, int childRow, int childCol)
+        {
+            return matrix[parentRow, parentCol] == matrix[childRow, childCol];
+        }
+
+        private static bool IsInside(int row, int col)
+        {
+            return row >= 0 &&
+                   row < matrix.GetLength(0) &&
+                   col >= 0 &&
+                   col < matrix.GetLength(1);
+        }
+
+        private static char[,] ReadMatrix(int rows, int cols)
+        {
+            var matrix = new char[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var line = Console.ReadLine();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = line[j];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ 06. Graph Theory, Traversal and Shortest Paths - Exercise/05. Break Cycles/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace _05._Break_Cycles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Edge
+    {
+        public Edge(string first, string second)
+        {
+            this.First = first;
+            this.Second = second;
+        }
+
+        public string First { get; set; }
+
+        public string Second { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.First} - {this.Second}";
+        }
+
+        public string ToStringReversed()
+        {
+            return $"{this.Second} - {this.First}";
+        }
+    }
+
+    public class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static List<Edge> edges;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            graph = new Dictionary<string, List<string>>();
+            edges = new List<Edge>();
+
+            ProcesInput(n);
+
+            edges = edges.OrderBy(e => e.First).ThenBy(e => e.Second).ToList();
+
+            var removeEdges = new List<Edge>();
+            var blackList = new HashSet<string>();
+
+            foreach (var edge in edges)
+            {
+                var first = edge.First;
+                var second = edge.Second;
+
+                graph[first].Remove(second);
+                graph[second].Remove(first);
+
+                if (HasPath(first, second))
+                {
+                    if (blackList.Contains(edge.ToString()))
+                    {
+                        continue;
+                    }
+                    removeEdges.Add(edge);
+                    blackList.Add(edge.ToStringReversed());
+                }
+                else
+                {
+                    graph[first].Add(second);
+                    graph[second].Add(first);
+                }
+            }
+
+            Console.WriteLine($"Edges to remove: {removeEdges.Count}");
+
+            
+            foreach (var edge in removeEdges)
+            {
+                Console.WriteLine(edge.ToString());
+            }
+        }
+
+        private static bool HasPath(string source, string destination)
+        {
+            var queue = new Queue<string>();
+            queue.Enqueue(source);
+
+            var visited = new HashSet<string> { source };
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node == destination)
+                {
+                    return true;
+                }
+
+                foreach (var child in graph[node])
+                {
+                    if (visited.Contains(child))
+                    {
+                        continue;
+                    }
+
+                    visited.Add(child);
+                    queue.Enqueue(child);
+                }
+            }
+
+            return false;
+        }
+
+        private static void ProcesInput(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                var parts = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                var node = parts[0];
+                var children = parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (!graph.ContainsKey(node))
+                {
+                    graph.Add(node, new List<string>());
+                }
+
+                foreach (var child in children)
+                {
+                    graph[node].Add(child);
+                    edges.Add(new Edge(node, child));
+                }
+            }
+        }
+    }
+}
