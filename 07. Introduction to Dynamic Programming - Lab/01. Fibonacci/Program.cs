d8bcd409f0b7e1a01387ec36e5a3b2f9767e1e9d
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2021-01-05 22:36:50 +0200

    Add 07. Introduction to Dynamic Programming - Lab

---
+++ 07. Introduction to Dynamic Programming - Lab/01. Fibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01._Fibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        private static Dictionary<int, long> memo;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            memo = new Dictionary<int, long>();
+
+            Console.WriteLine(GetFibonacci(n));
+        }
+
+        private static long GetFibonacci(int n)
+        {
+            if (memo.ContainsKey(n))
+            {
+                return memo[n];
+            }
+
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            var result = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+
+            memo[n] = result;
+
+            return result;
+        }
+    }
+}
---
+++ 07. Introduction to Dynamic Programming - Lab/03. Longest Common Subsequence/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _03._Longest_Common_Subsequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstWord = Console.ReadLine();
+            var secondWord = Console.ReadLine();
+
+            var table = new int[firstWord.Length + 1, secondWord.Length + 1];
+
+            for (int r = 1; r < table.GetLength(0); r++)
+            {
+                for (int c = 1; c < table.GetLength(1); c++)
+                {
+                    if (firstWord[r-1] == secondWord[c -1])
+                    {
+                        table[r, c] = table[r - 1, c - 1] + 1;
+                    }
+                    else
+                    {
+                        table[r, c] = Math.Max(table[r - 1, c], table[r, c - 1]);
+                    }
+                }
+            }
+
+            Console.WriteLine(table[firstWord.Length, secondWord.Length]);
+
+            //var row = firstWord.Length;
+            //var col = secondWord.Length;
+
+            //var stack = new Stack<char>();
+
+            //while (row > 0 && col > 0)
+            //{
+            //    if (firstWord[row-1] == secondWord[col-1])
+            //    {
+            //        row--;
+            //        col--;
+
+            //        stack.Push(firstWord[row]);
+            //    }
+            //    else if (table[row-1, col]> table[row, col-1])
+            //    {
+            //        row--;
+            //    }
+            //    else
+            //    {
+            //        col--;
+            //    }
+            //}
+
+            //Console.WriteLine(string.Join(" ", stack));
+        }
+    }
+}
