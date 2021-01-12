d39a6ad6bfa2bac402efc85bcb01f7e804236a69
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2021-01-12 22:00:45 +0200

    Add Exam Preparation

---
+++ Exam Preparation/02. Time/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _02._Time
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var positions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var table = new int[numbers.Length + 1, positions.Length + 1];
+
+            for (int r = 1; r < table.GetLength(0); r++)
+            {
+                for (int c = 1; c < table.GetLength(1); c++)
+                {
+                    if (numbers[r - 1] == positions[c - 1])
+                    {
+                        table[r, c] = table[r - 1, c - 1] + 1;
+                    }
+                    else
+                    {
+                        table[r, c] = Math.Max(table[r - 1, c], table[r, c - 1]);
+                    }
+                }
+            }
+
+            var row = numbers.Length;
+            var col = positions.Length;
+
+            var stack = new Stack<int>();
+
+            while (row > 0 && col > 0)
+            {
+                if (numbers[row - 1] == positions[col - 1])
+                {
+                    row--;
+                    col--;
+
+                    stack.Push(numbers[row]);
+                }
+                else if (table[row - 1, col] > table[row, col - 1])
+                {
+                    row--;
+                }
+                else
+                {
+                    col--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", stack));
+            Console.WriteLine(table[numbers.Length, positions.Length]);
+        }
+    }
+}
---
+++ Exam Preparation/03. The Story Telling/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._The_Story_Telling
+{
+    public class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static HashSet<string> visited;
+        private static Stack<string> stack;
+
+        static void Main(string[] args)
+        {
+            graph = ReadGraph();
+            visited = new HashSet<string>();
+            stack = new Stack<string>();
+
+            foreach (var node in graph.Keys)
+            {
+                DFS(node);
+            }
+
+            Console.WriteLine(string.Join(" ", stack));
+        }
+
+        private static void DFS(string node)
+        {
+            if (visited.Contains(node))
+            {
+                return;
+            }
+
+            visited.Add(node);
+
+            foreach (var child in graph[node])
+            {
+                DFS(child);
+            }
+
+            stack.Push(node);
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph()
+        {
+            var graphResult = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "End")
+                {
+                    break;
+                }
+
+                var parts = line.Split("->", StringSplitOptions.RemoveEmptyEntries);
+
+                var preStory = parts[0].Trim();
+
+                if (parts.Length == 1)
+                {
+                    graphResult[preStory] = new List<string>();
+                }
+                else
+                {
+                    var children = parts[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                    graphResult[preStory] = children;
+                }
+            }
+
+            return graphResult;
+        }
+    }
+}
