4fc201ee9baef7ad5783f4596b6f237cfa8f07ee
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2021-01-14 00:08:23 +0200

    Add Exam 09.01.2020

---
+++ Exam 09.01.2020/01. Super Set/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01._Super_Set
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static string[] combinations;
+        private static int k;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(", ");
+            k = elements.Length;
+
+            for (int i = 0; i <= k; i++)
+            {
+                combinations = new string[i];
+
+                Combinations(0, 0);
+            }
+        }
+
+        private static void Combinations(int combIndex, int elementsStartIndex)
+        {
+            if (combIndex >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementsStartIndex ; i < elements.Length; i++)
+            {
+                combinations[combIndex] = elements[i];
+                Combinations(combIndex + 1, i + 1);
+            }
+        }
+    }
+}
---
+++ Exam 09.01.2020/02. Paths/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace _02._Paths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        private static Dictionary<int, List<int>> graph;
+        private static bool[] visited;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            visited = new bool[n+1];
+
+            graph = ReadGraph(n);
+
+            for (int node = 0; node < graph.Count; node++)
+            {
+                var component = new List<int>();
+                DFS(node, component);
+            }
+        }
+
+        private static Dictionary<int, List<int>> ReadGraph(int nodesCount)
+        {
+            var result = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < nodesCount; i++)
+            {
+                var node = i;
+
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    result[node] = new List<int>();
+                    continue;
+                }
+
+                var children = line
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToList();
+
+                result[node] = children;
+            }
+
+            return result;
+        }
+
+        private static void DFS(int node, List<int> component)
+        {
+            if (component.Contains(node))
+            {
+                return;
+            }
+
+            component.Add(node);
+
+            foreach (var child in graph[node])
+            {
+                DFS(child, component);
+                if (graph[child].Count == 0)
+                {
+                    Console.WriteLine(string.Join(" ", component));
+                }
+                
+                var last = component.Last();
+                component.Remove(last);
+            }
+        }
+    }
+}
