f20b20cd52dd45d5ce7a4951a64f4741aa3c0737
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2021-02-18 23:00:15 +0200

    Add "Dynamic Programming Advanced - Exercise"

---
+++ Algorithms Advanced/Dynamic Programming Advanced - Exercise/01. Cable Merchant/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _01._Cable_Merchant
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        private static List<int> prices;
+        private static int[] bestPrices;
+
+        static void Main(string[] args)
+        {
+            prices = new List<int> { 0 };
+
+            prices.AddRange(Console.ReadLine().Split().Select(int.Parse));
+
+            var connectorPrice = int.Parse(Console.ReadLine());
+
+            bestPrices = new int[prices.Count];
+
+            var result = new List<int>();
+
+            for (int length = 1; length < prices.Count; length++)
+            {
+                var bestprices = CutCable(length, connectorPrice);
+
+                result.Add(bestprices);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static int CutCable(int length, int connectorPrice)
+        {
+            if (length == 0)
+            {
+                return 0;
+            }
+
+            if (bestPrices[length] != 0)
+            {
+                return bestPrices[length];
+            }
+
+            var bestPrice = prices[length];
+
+            for (int i = 1; i < length; i++)
+            {
+                var currPrice = prices[i] + CutCable(length - i, connectorPrice) - 2 * connectorPrice;
+
+                if (currPrice > bestPrice)
+                {
+                    bestPrice = currPrice;
+                }
+            }
+
+            bestPrices[length] = bestPrice;
+
+            return bestPrice;
+        }
+    }
+}
---
+++ Algorithms Advanced/Dynamic Programming Advanced - Exercise/02. Battle Points/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _02._Battle_Points
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var requiredEnergy = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var battlePoints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var enemies = requiredEnergy.Length;
+
+            var initialEnergy = int.Parse(Console.ReadLine());
+
+            var dp = new int[enemies + 1, initialEnergy +1];
+            //var included = new bool[enemies + 1, initialEnergy + 1];
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                var enemyIdx = row - 1;
+                var enemyEnergy = requiredEnergy[enemyIdx];
+                var enemyBattlePoints = battlePoints[enemyIdx];
+
+                for (int energy = 1; energy < dp.GetLength(1); energy++)
+                {
+                    var skip = dp[row - 1, energy];
+                    if (enemyEnergy > energy)
+                    {
+                        dp[row, energy] = skip;
+                        continue;
+                    }
+
+                    var take = enemyBattlePoints + dp[row - 1, energy - enemyEnergy];
+
+                    //if (take > skip)
+                    //{
+                    //    dp[row, energy] = take;
+                    //    included[row, energy] = true;
+                    //}
+                    //else
+                    //{
+                    //    dp[row, energy] = skip;
+                    //}
+
+                    dp[row, energy] = Math.Max(skip, take);
+                }
+            }
+
+            Console.WriteLine(dp[enemies, initialEnergy]);
+
+            //var currEnergy = initialEnergy;
+
+            //for (int row = enemies; row >= 0; row--)
+            //{
+            //    if (included[row, currEnergy])
+            //    {
+            //        var enemyInx = row - 1;
+
+            //        Console.WriteLine(enemyInx);
+            //        currEnergy -= requiredEnergy[enemyInx];
+
+            //        if (currEnergy == 0)
+            //        {
+            //            break;
+            //        }
+            //    }
+            //}
+        }
+    }
+}
