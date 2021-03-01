277db0deebad19a7d56bbd38582648753f7e0cd2
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2021-03-01 15:14:41 +0200

    Add Exam 20.02.2021
    01. Black Friday - 100/100
    02. Boxes - 100/100
    03. Emergency Plan - 100/100

---
+++ Algorithms Advanced/Exam 20.02.2021/01. Black Friday/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace _01._Black_Friday
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Wintellect.PowerCollections;
+
+    public class Edge
+    {
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    public class Program
+    {
+        private static Dictionary<int, List<Edge>> edgesByNode;
+        private static HashSet<int> forest;
+        private static int result;
+
+        static void Main(string[] args)
+        {
+            var shops = int.Parse(Console.ReadLine());
+            var streets = int.Parse(Console.ReadLine());
+
+            edgesByNode = ReadGraph(shops, streets);
+            forest = new HashSet<int>();
+            result = 0;
+
+            foreach (var node in edgesByNode.Keys)
+            {
+                if (!forest.Contains(node))
+                {
+                    Prim(node);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static void Prim(int node)
+        {
+            forest.Add(node);
+
+            var queue = new OrderedBag<Edge>(edgesByNode[node], Comparer<Edge>.Create((f, s) => f.Weight - s.Weight));
+
+            while (queue.Count > 0)
+            {
+                var edge = queue.RemoveFirst();
+                var nonTreeNode = GetNonTreeNode(edge.First, edge.Second);
+
+                if (nonTreeNode == -1)
+                {
+                    continue;
+                }
+
+                result += edge.Weight;
+
+                forest.Add(nonTreeNode);
+                queue.AddMany(edgesByNode[nonTreeNode]);
+            }
+        }
+
+        private static int GetNonTreeNode(int first, int second)
+        {
+            var nonTreeNode = -1;
+
+            if (forest.Contains(first) && !forest.Contains(second))
+            {
+                nonTreeNode = second;
+            }
+            else if (!forest.Contains(first) && forest.Contains(second))
+            {
+                nonTreeNode = first;
+            }
+
+            return nonTreeNode;
+        }
+
+        private static Dictionary<int, List<Edge>> ReadGraph(int shops, int streets)
+        {
+            var result = new Dictionary<int, List<Edge>>();
+
+            for (int i = 0; i < streets; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var firstNode = edgeData[0];
+                var secondNode = edgeData[1];
+                var weight = edgeData[2];
+
+                if (!result.ContainsKey(firstNode))
+                {
+                    result.Add(firstNode, new List<Edge>());
+                }
+
+                if (!result.ContainsKey(secondNode))
+                {
+                    result.Add(secondNode, new List<Edge>());
+                }
+
+                var edge = new Edge
+                {
+                    First = firstNode,
+                    Second = secondNode,
+                    Weight = weight,
+                };
+
+                result[firstNode].Add(edge);
+                result[secondNode].Add(edge);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms Advanced/Exam 20.02.2021/02. Boxes/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace _02._Boxes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public class Box
+        {
+            public int Width { get; set; }
+
+            public int Depth { get; set; }
+
+            public int Height { get; set; }
+
+            public override string ToString()
+            {
+                return $"{this.Width} {this.Depth} {this.Height}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var linesCount = int.Parse(Console.ReadLine());
+            var boxes = new List<Box>();
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                var box = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                boxes.Add(new Box
+                {
+                    Width = box[0],
+                    Depth = box[1],
+                    Height = box[2],
+                });
+            }
+
+            var len = new int[boxes.Count];
+            var prev = new int[boxes.Count];
+
+            var bestLen = 0;
+            var lastIndex = 0;
+
+            for (int i = 0; i < boxes.Count; i++)
+            {
+                prev[i] = -1;
+
+                var currNumber = boxes[i];
+                var currBestSeq = 1;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    var prevNumber = boxes[j];
+
+                    if (prevNumber.Width < currNumber.Width &&
+                        prevNumber.Depth < currNumber.Depth && 
+                        prevNumber.Height < currNumber.Height && 
+                        len[j] + 1 >= currBestSeq)
+                    {
+                        currBestSeq = len[j] + 1;
+                        prev[i] = j;
+                    }
+                }
+
+                if (currBestSeq > bestLen)
+                {
+                    bestLen = currBestSeq;
+                    lastIndex = i;
+                }
+
+                len[i] = currBestSeq;
+            }
+
+            var stack = new Stack<Box>();
+
+            while (lastIndex != -1)
+            {
+                stack.Push(boxes[lastIndex]);
+                lastIndex = prev[lastIndex];
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
